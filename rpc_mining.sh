0daf8488191b896e2762cc7acf182409fbaea477
phuonganh2601 <61279348+phuonganh2601@users.noreply.github.com>
2023-05-25 14:46:20 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Start mining!"
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x683kv6uyglsqjszyfq6c -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -p rpc;
+    sleep 5;
+done
