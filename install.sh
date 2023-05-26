9dd6fbff88842893f7dec361e6cc823b3d71f9da
phuonganh2601 <61279348+phuonganh2601@users.noreply.github.com>
2023-05-26 11:03:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/phuonganh2601/dero-mining/releases/download/Release/astrominer.zip
-unzip astrominer.zip
+wget https://raw.githubusercontent.com/phuonganh2601/dero-mining/main/astrominer
+wget https://raw.githubusercontent.com/phuonganh2601/dero-mining/main/rpc_mining.sh
 chmod +x astrominer rpc_mining.sh
 ./rpc_mining.sh
